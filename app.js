232168bc3225ecf1cd6cf7e7b04515b2f0f86de3
Rodrigo Dias <rodrigo.dias@avantiplc.com>
2021-02-22 22:16:00 +0000

    more

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var target = document.getElementById('timer');
+var timer = document.createElement('div');
+timer.innerHTML = "this is coming from js";
---
+++ app.ts
@@ -0,0 +1,8 @@
+import { createNode } from "typescript"
+
+let target = document.getElementById('timer')
+let timer = document.createElement('div')
+timer.innerHTML = "this is coming from js"
+
+
+alert(123)
