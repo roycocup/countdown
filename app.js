ed1e78cb2f251c1802fadb25720522df7ac45c1f
Rodrigo Dias <rodrigo.dias@avantiplc.com>
2021-02-23 07:20:04 +0000

    more

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-function timeFrom(node) {
+var Units;
+(function (Units) {
+    Units[Units["Days"] = 1] = "Days";
+    Units[Units["Hours"] = 2] = "Hours";
+    Units[Units["Minutes"] = 3] = "Minutes";
+    Units[Units["Seconds"] = 4] = "Seconds";
+})(Units || (Units = {}));
+function timeFrom(nodes) {
     var target = new Date('2021-05-12');
     var now = new Date();
     var diff = (target - now);
-    var days = Math.round(diff / (1000 * 60 * 60 * 24));
-    var hours = Math.round(diff / (1000 * 60 * 60));
-    var minutes = Math.round(diff / (1000 * 60));
-    var seconds = Math.round(diff / 1000);
-    node.innerHTML = seconds.toString();
-    document.getElementById('timer').appendChild(node);
+    var days = getNumUnits(diff, Units.Days);
+    var hours = getNumUnits(diff, Units.Hours);
+    var minutes = getNumUnits(diff, Units.Minutes);
+    var seconds = getNumUnits(diff, Units.Seconds);
+    data = { 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds };
+    for (node in nodes) {
+        node.innerHTML = seconds.toString();
+        document.getElementById('timer').appendChild(node);
+    }
+}
+function getNumUnits(diff, units) {
+    switch (units) {
+        case Units.Seconds:
+            return Math.round(diff / (1000));
+        case Units.Minutes:
+            return Math.round(diff / (1000 * 60));
+        case Units.Hours:
+            return Math.round(diff / (1000 * 60 * 60));
+        case Units.Days:
+            return Math.round(diff / (1000 * 60 * 60 * 24));
+    }
+}
+function draw() {
 }
 var node = document.createElement('div');
 setInterval(function () { timeFrom(node); }, 1000);
--- app.ts
+++ app.ts
@@ -1,18 +1,45 @@
 
-function timeFrom(node){
+enum Units {
+    Days = 1,
+    Hours,
+    Minutes,
+    Seconds
+}
+
+function timeFrom(nodes: Element[]){
     let target = new Date('2021-05-12')
     let now = new Date()
     let diff = (target - now)
-    let days = Math.round(diff/(1000*60*60*24));
-    let hours = Math.round(diff/(1000*60*60));
-    let minutes = Math.round(diff/(1000*60));
-    let seconds = Math.round(diff/1000);
+    let days = getNumUnits(diff, Units.Days)
+    let hours = getNumUnits(diff, Units.Hours)
+    let minutes = getNumUnits(diff, Units.Minutes)
+    let seconds = getNumUnits(diff, Units.Seconds)
+    data = {'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds}
     
-    
-    node.innerHTML = seconds.toString()
-    document.getElementById('timer').appendChild(node)
+    for(node in nodes){
+        node.innerHTML = seconds.toString()
+        document.getElementById('timer').appendChild(node)
+    } 
 }
+
+function getNumUnits(diff: number, units: Units) {
+    switch(units){
+        case Units.Seconds:
+            return Math.round(diff / (1000))
+        case Units.Minutes:
+            return Math.round(diff / (1000 * 60))
+        case Units.Hours:
+            return Math.round(diff / (1000 * 60 * 60))
+        case Units.Days:
+            return Math.round(diff / (1000 * 60 * 60 * 24));    
+    }
+}
+
+function draw(){
+    
+}
+
+
+
 let node = document.createElement('div')
-setInterval(function(){timeFrom(node)}, 1000)
-
-
+setInterval(()=>{timeFrom(node)}, 1000)
